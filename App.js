fe65095b488159b21cf285f8402dd7a301acab96
satish7654 <satishsingh202dd+@gmail.com>
2024-10-05 12:31:39 +0545

    third

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1", {}, "Satish React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
